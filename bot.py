43aea0326fccce5168f33a3f63e53d739dce1e91
pete <peterwaterland@gmail.com>
2016-01-24 19:29:56 +0000

    code

---
+++ bot.py
@@ -0,0 +1,89 @@
+__author__ = 'pete'
+
+import quickstart
+import time
+import praw
+import sys
+import schedule
+import os
+import random
+
+USERNAME = ''       #add in login details for bot..
+PASSWORD = ''
+
+downvoters = []     #usernames to downvote and upvote..
+upvoters = []
+
+n_upvotes = 0
+n_downvotes = 0
+
+already_comment_id = []
+
+def log_work():
+    global n_upvotes, n_downvotes
+    print "Emailing 24 hourly log file.."
+    print 'Upvotes ',str(n_upvotes),' Downvotes ', str(n_downvotes)
+    quickstart.CredsSendLog(n_upvotes, n_downvotes)
+    os.rename('log.txt','log.old'+str(random.randint(0,65535)))
+    n_upvotes = 0
+    n_downvotes = 0
+    print "Saving old log file, creating new file.."
+    log(time.strftime("%Y-%m-%d %H:%M:%S")+' New daily log begins.. \n')
+    #sends the log.txt to my email address and moves the log to a new file..
+    return
+
+def log(string_data):
+    with open("log.txt", "a") as myfile:
+        myfile.write(string_data)
+    return
+
+
+
+r = praw.Reddit(user_agent='Something random')          #change this to a unique string..
+r.login(USERNAME, PASSWORD, disable_warning=True)
+#r.login(USERNAME, PASSWORD)
+subreddit = r.get_subreddit('test')                     #a subreddit to monitor..
+#subreddit_comments = subreddit.get_comments()
+
+print '>>>Reddit /r/bitcoin bot..<<<'
+
+print 'Downvoting:', downvoters
+print 'Upvoting:', upvoters
+
+print '>>>active - logging to /log.txt<<<'
+
+schedule.every().day.at("07:00").do(log_work)           #scheduled log maintenance and emailing..
+schedule.every().day.at("19:00").do(log_work)
+
+while True:
+
+
+    subreddit_comments = subreddit.get_comments()       #get the new comments..
+
+
+    for comment in subreddit_comments:                  #make sure they havent been seen already..
+        
+        if comment.id in already_comment_id:
+         pass
+       
+        else:
+            
+            already_comment_id.append(comment.id)
+
+            if comment.author.name in downvoters:
+               print "downvoted: ", comment.author.name, comment.id
+               comment.downvote()
+               n_downvotes+=1
+               logstr = time.strftime("%Y-%m-%d %H:%M:%S")+' downvoted '+comment.author.name+' '+comment.id+'\n'
+               log(logstr)
+            if comment.author.name in upvoters:
+                print 'match', comment.author.name, comment.id
+                logstr = time.strftime("%Y-%m-%d %H:%M:%S")+' upvoted '+comment.author.name+' '+comment.id+'\n'
+                log(logstr)
+                comment.upvote()
+                n_upvotes+=1
+
+    sys.stdout.write('.')
+    sys.stdout.flush()
+    schedule.run_pending()
+    time.sleep(random.randint(0,20))            #randomly wait before scanning subreddit again..
---
+++ quickstart.py
@@ -0,0 +1,79 @@
+from __future__ import print_function
+import httplib2
+import os
+import gmaily           #contains the gmail send message functions..
+
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://mail.google.com'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gmail API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'gmail-python-quickstart.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def CredsSendLog(ups,downs):
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+    mail_msg = gmaily.CreateMessageWithAttachment(sender='something@gmail.com', to='something@something.com', subject='Reddit bot log', message_text='Activity log attached.\nTotal downvotes='+str(downs)+'.\nTotal upvotes='+str(ups)+'.', file_dir='/', filename='log.txt')
+    return gmaily.SendMessage(service=service,user_id='something@something.com',message=mail_msg)
+
+
+def main():
+    """Shows basic usage of the Gmail API.
+
+    Creates a Gmail API service object and outputs a list of label names
+    of the user's Gmail account.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+
+    if not labels:
+        print('No labels found.')
+    else:
+      print('Labels:')
+      for label in labels:
+        print(label['name'])
+
+if __name__ == '__main__':
+    main()
